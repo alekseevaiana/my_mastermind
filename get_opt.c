aac663313d52bdab1d842c504fb282fab236509e
iana alekseeva <aleksee_i@qwasar.io>
2022-05-06 18:15:02 +0000

    comments on what to check in options

--- get_opt.c
+++ get_opt.c
@@ -59,7 +59,8 @@ t_opt* get_opt(int ac, char** av)
     {
         if (is_option(av[index]) == 1)
         {
-            check_option_value(av[index + 1]); // if option value contains only digits!            
+            check_option_value(av[index + 1]); // if option value contains only digits!
+            // also check if there is an option value aftre option         
             set_option(opt, av[index] + 1, av[index + 1]); // av[index] + 1 -> is a pointer that points to next char in a str
         }
         index += 1;
