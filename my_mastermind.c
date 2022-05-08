b64e6a6f43de12eab7c2454defcf7869f5668514
iana alekseeva <aleksee_i@qwasar.io>
2022-05-08 00:33:09 +0000

    main functionality

--- my_mastermind.c
+++ my_mastermind.c
@@ -10,23 +10,29 @@ typedef struct t_guess_result
     int missplaced_amount;
 } t_guess_result;
 
+// Function that compares secret and answer and return number of well placed and missplaced
+// Function expects inputs are valid
 t_guess_result check_answer(char* secret, char* answer) {
     t_guess_result result;
+
+    result.missplaced_amount = 1;
+    result.well_placed_amount = 3;
+    printf("%c", secret[0]);
+    printf("%c", answer[0]);
+    // TODO: all
 
     return result;
 }
 
 char* read_answer()
 {
-    char buff;
     char* input;
+    int result;
     
     input = malloc(sizeof(char) * EXPECTED_INPUT_LENGTH);
    
-    int read_size = 0;
-
     write(0, "> ", 2);
-    int result = read(STDIN_FILE_DESCRIPTOR, input, EXPECTED_INPUT_LENGTH);
+    result = read(STDIN_FILE_DESCRIPTOR, input, EXPECTED_INPUT_LENGTH);
     // Check user entered at lest minimal amount and no error happened
     if (result < EXPECTED_INPUT_LENGTH) {
         return NULL;
@@ -36,23 +42,26 @@ char* read_answer()
         return NULL;
     } 
 
+    input[EXPECTED_INPUT_LENGTH - 1] = '\0';
+
     return input;
 }
 
-bool is_answer_valid(char *answer) {
-    // TODO: add validation
+bool is_code_valid(char *code) {
+    if (strlen(code) != EXPECTED_ANSWER_LENGTH) {
+        return false;
+    }
+    // TODO: add validation for each char
     return true;
 }
 
 
-
 int main(int ac, char** av) {
     t_opt* options = get_opt(ac, av);
-    if (strlen(options->c_val) != EXPECTED_ANSWER_LENGTH) {
-        printf("Secret length should be %d", EXPECTED_ANSWER_LENGTH);
+    if (!is_code_valid(options->c_val) ) {
+        printf("secret is invalid");
         return 1;
     }
-    // TODO: CHECK SECRET CHARS
 
     printf("====> Will you find the secret code?\n");
     printf("---\n");
@@ -63,7 +72,7 @@ int main(int ac, char** av) {
 
     while (attempt < options->t_val) {
         answer = read_answer();
-        if (answer != NULL && is_answer_valid(answer)) {
+        if (answer != NULL && is_code_valid(answer)) {
             guess = check_answer(options->c_val, answer);
             
             // WHEN WON
@@ -72,23 +81,16 @@ int main(int ac, char** av) {
                 return 0;
             }
 
-
-
-            // DO
+            printf("Well placed pieces: %d\n", guess.well_placed_amount);
+            printf("Misplaced pieces: %d\n", guess.missplaced_amount);
         } else {
             printf("Wrong input!");
         }
         free(answer);
     }
 
-    // TODO: add random generation
-
-    // run loop on every attempt (-t). 10 attempts by default
-    // read input from user
-    //          -> must be 4 digits every one is from 0 - 7
-    // store input in a struct or an object
-    // compare input and secret code
-    return 0;
+    printf("Exeded amount of attempts");
+    return 1;
 }
 
 
